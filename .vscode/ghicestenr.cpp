7d52069f331564be96a8393e5038e712fddd18d5
melutz-finutz <melany00leona@gmail.com>
2026-04-10 13:58:15 +0300

    ghicestenrjoc

--- .vscode/ghicestenr.cpp
+++ .vscode/ghicestenr.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main() {
-    
     int n, k = 0;
 
     random_device rd;
@@ -11,32 +10,7 @@ int main() {
     uniform_int_distribution<> dist(1, 100);
 
     int num = dist(gen);
-
-    cout << "Guess a number between 1 and 100:" << endl;
-
-    do {
-        cin >> n;
-        k++;
-
-        if (n > num) {
-            cout << "Too big" << endl;
-        } 
-        else if (n < num) {
-            cout << "#include <iostream>
-#include <random>
-using namespace std;
-
-int main() {
-    int n, k = 0;
-
-    // Random number setup
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dist(1, 100);
-
-    int num = dist(gen);
-
-    cout << "Guess a number between 1 and 100:" << endl;
+    cout << "Random number generated!" << endl;
 
     do {
         cin >> n;
@@ -49,16 +23,7 @@ int main() {
             cout << "Too small" << endl;
         } 
         else {
-            cout << "You did it in " << k << " moves!" << endl;
-        }
-
-    } while (n != num);
-
-    return 0;
-}" << endl;
-        } 
-        else {
-            cout << "You did it in " << k << " moves!" << endl;
+            cout << "You did it in " << k << " moves" << endl;
         }
 
     } while (n != num);
